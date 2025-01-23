aad1699bf8e096233595ef72950698fc0c1b5b7a
Aneroscode <frankabbey2000@gmail.com>
2025-01-23 00:29:31 +0000

    json server
    created a json server

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,9 +1,32 @@
+import axios from 'axios'
 import React from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Create = () => {
 
-  
+  const [inputData, setInputData] = useState({
 
+    details : "",
+    merchant : "",
+    amount : "",
+    report : "",
+    status : "",
+    
+  })
+
+  const navigat = useNavigate()
+
+  function handleSubmit(event) { 
+
+    event.preventDefault()
+    axios.post('http://localhost:3031/data', inputData)
+    .then(res => {
+        alert("Expense Added Successfully")
+        navigat('/')
+    }).catch (err => console.log(err))
+
+  }
 
   return (
     <div className='bg-white w-full h-100vh flex justify-center items-center'>
@@ -11,11 +34,11 @@ const Create = () => {
         <div className='pt-7 pb-7 rounded shadow-lg w-4/5 p-4 px-10 '>
             <h1 className='text-4xl font-bold' >Add New Expenses</h1>
 
-            <form  className='mt-4'>
+            <form onSubmit={handleSubmit}  className='mt-4'>
 
                 <div className='mb-2 '>
                     <label className='font-semibold text-lg' >Details:</label>
-                    <input className=' p-2 w-full border-2 rounded-lg border-gray-200' type='text' name='name' placeholder='Enter your details'
+                    <input className=' p-2 w-full border-2 rounded-lg border-gray-200' type='text' name='details' placeholder='Enter your details'   onChange={e => setInputData({...inputData, details:e.target.value})}
                      />
                 
                 </div>
@@ -24,32 +47,41 @@ const Create = () => {
 
                 <div className='mb-2'>
                     <label className='font-semibold text-lg'>Merchant:</label>
-                    <input className='p-2 w-full border-2 rounded-lg border-x-gray-200' type='text' name='name' placeholder='Enter your merchant'
-                     />
+                    <input className='p-2 w-full border-2 rounded-lg border-x-gray-200' type='text' name='merchant' placeholder='Enter your merchant'
+                      
+                      onChange={e => setInputData({...inputData, merchant: e.target.value})}/>
                 </div>
 
 
                 <div className='mb-2'>
                     <label className='font-semibold text-lg'>Amount:</label>
-                    <input className='p-2 w-full border-2 rounded-lg border-x-gray-200' type='text' name='name' placeholder='Enter your amount'
-                     />
+                    <input className='p-2 w-full border-2 rounded-lg border-x-gray-200' type='text' name='amount' placeholder='Enter your amount'
+                      
+                      onChange={e => setInputData({...inputData, amount: e. target.value})} />
                 </div>
 
                 <div className='mb-2'>
                     <label className='font-semibold text-lg'>Report:</label>
-                    <input className='p-2 w-full border-2 rounded-lg border-x-gray-200' type='text' name='name' placeholder='Enter your Report'
-                     />
+                    <input className='p-2 w-full border-2 rounded-lg border-x-gray-200' type='text' name='report' placeholder='Enter your Report'
+                       
+                       onChange={e => setInputData({...inputData, report: e.target.value})}
+                       />
                 </div>
 
              
 
                 <div className='mb-2'>
                     <label className='font-semibold text-lg'>Status:</label>
-                    <input className='p-2 w-full border-2 rounded-lg border-x-gray-200' type='text' name='name' placeholder='Enter your Status'
+                    <input className='p-2 w-full border-2 rounded-lg border-x-gray-200' type='text' name='status' placeholder='Enter your Status'
+                      
+                      onChange={e => setInputData({...inputData, status: e.target.value})}
+                     
                      />
                 </div>
 
-                <button className='px-8 py-2 bg-green-600 font-semibold text-white shadow-md   rounded-xl mt-2 hover:bg-green-500 active:bg-green-400'>Submit</button>
+                <button
+                    
+                className='px-8 py-2 bg-green-600 font-semibold text-white shadow-md   rounded-xl mt-2 hover:bg-green-500 active:bg-green-400'>Submit</button>
             </form>
 
         </div>
--- src/pages/Expenses.jsx
+++ src/pages/Expenses.jsx
@@ -3,46 +3,24 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import DataTable from 'react-data-table-component'
-import Data from '../components/data.json'
+
 import { Link } from 'react-router-dom'
 
 const Expenses = () => {
 
-  // const column =[
-  //   {
-  //     name: "ID",
-  //     selector : row => row.id 
-  //   },
-  //   {
-  //     name: "NAME",
-  //     selector : row => row.name
-  //   },
-  //   {
-  //     name: "EMAIL",
-  //     selector : row => row.email
-  //   },
-  //   {
-  //     name: "CITY",
-  //     selector : row => row.address.city
-  //   },
-  //   {
-  //     name: "USERNAME",
-  //     selector : row => row.username
-  //   }
-  // ]
+  const [data, setData] = useState([])
 
-  //     useEffect(() => {
-  //       const fetchData = async () => {
-  //         axios.get('https://jsonplaceholder.typicode.com/users')
-  //         .then(res => setRecords(res.data))
-  //         .catch(err => console.log(err))
-  //       }
-  //       fetchData()
-  //     },[])
-
-  //     const [records, setRecords] = useState([])
+      useEffect(() => {
+          axios.get('http://localhost:3031/data')
+          .then(res => setData(res.data))
+          .catch(err => console.log(err))
+        }
+        
+      ,[])
+ console.log(data)
+ 
   const [status, setStatus] = useState(false)
-  const [data, setData] = useState(Data)
+  
 
   return (
     <div className=' px-4 py-6'>
@@ -103,178 +81,10 @@ const Expenses = () => {
       }
       </table>
 
-      {/* <div className=' flex justify-between   py-2'>
-        <h1 className='text-2xl md:3xl lg:4xl font-bold'>Expenses</h1>
-        <div className='flex space-x-4'>
-          <button className='py-1 px-4 rounded-xl font-bold hover:scale-110'>+New expense</button>
-          <div className='flex mt-2 space-x-3 '>
-            <FontAwesomeIcon className=' py-1 px-1 rounded-lg hover:scale-110' icon={faNetworkWired}/>
-            <FontAwesomeIcon className=' py-1 px-1 rounded-lg hover:scale-110' icon={faBars}/>
-            <FontAwesomeIcon className=' py-1 px-1 rounded-lg hover:scale-110' icon={faListDots}/>
-          </div>
-        </div>
-      </div> */}
+     
 
-      {/* <div className=''>
-      <DataTable
-      
-      columns  = {column}
-        data = {records}
-      
-      ></DataTable>
-      </div> */}
-
-      {/* <div > 
-        <table  className='w-full'>
-          <thead className=' bg-gray-50 border-b-2 border-gray-200  '>
-            <tr>
-              <th className='p-3 text-sm font-semibold -tracking-wide text-left' >DETAILS</th>
-              <th  className='p-3 text-sm font-semibold -tracking-wide text-left'  >MERCHANT</th>
-              <th  className='p-3 text-sm font-semibold -tracking-wide text-left' >AMOUNT</th>
-              <th  className='p-3 text-sm font-semibold -tracking-wide text-left' >REPORT</th>
-              <th  className='p-3 text-sm font-semibold -tracking-wide text-left'  >STATUS</th>
-            </tr>
-          </thead>
-
-
-          <tbody >
-            <tr className='bg-white' >
-              <td className='p-3 text-sm text-gray-700' >
-                <div className='flex items-centers gap-2 '>
-                  <input type="checkbox" name='checkbox' id='checkbox' />
-                  <FontAwesomeIcon className='mt-1 rounded-full bg-opacity-50 bg-yellow-300 px-2 py-2' icon={faBurger}/>
-                  <div>
-                    <span>09/11/2022</span>
-                    <h4>Food Catering</h4>
-                  </div>
-                </div>
-                </td> 
-              <td className='p-3 text-sm text-gray-700'>McFood</td> 
-              <td className='p-3 text-sm text-gray-700'>250</td> 
-              <td className='p-3 text-sm text-gray-700'>November_2022</td> 
-              <td className='p-3 text-sm text-gray-700'>
-                <span className=' cursor-pointer p-1.5 font-medium text-xs uppercase -tracking-wider text-blue-800 bg-blue-200 rounded-lg bg-opacity-50'>Not Submitted</span>
-                </td> 
-            </tr>
-
-            <tr className='bg-gray-50'>
-              <td className='p-3 text-sm text-gray-700'>
-               <div className='flex items-center gap-2'>
-                 <input type="checkbox" name='checkbox' id='checkbox' />
-                <FontAwesomeIcon className='mt-1 rounded-full bg-opacity-50 bg-yellow-300 px-2 py-2' icon={faScissors}/>
-                  <div>
-                    <span>09/11/2022</span>
-                    <h4>Office Supplies</h4>
-                  </div>
-                </div>
-              </td>
-              <td className='p-3 text-sm text-gray-700'>Officio</td> 
-              <td className='p-3 text-sm text-gray-700'>150</td> 
-              <td className='p-3 text-sm text-gray-700'>November_2022</td> 
-              <td className='p-3 text-sm text-gray-700'>
-                <span className='p-1.5 font-medium text-xs uppercase -tracking-wider text-blue-800 bg-blue-200 rounded-lg bg-opacity-50'>Not Submitted</span>
-                </td> 
-            </tr>
-
-            <tr className='bg-white'>
-
-              <td className='p-3 text-sm text-gray-700'>
-            <div className='flex items-center gap-2'>
-               <input type="checkbox" name='checkbox' id='checkbox' />
-              <FontAwesomeIcon className='mt-1 rounded-full bg-opacity-50 bg-yellow-300 px-2 py-2' icon={faBurger}/>
-                  <div>
-                    <span>09/11/2022</span>
-                    <h4>Business Launch</h4>
-                  </div>
-                </div>
-                </td>
-              <td  className='p-3 text-sm text-gray-700'>Resturant</td> 
-              <td  className='p-3 text-sm text-gray-700'>75.50</td> 
-              <td  className='p-3 text-sm text-gray-700'>November_2022</td> 
-              <td  className='p-3 text-sm text-gray-700'>
-                <span className='p-1.5 font-medium text-xs uppercase -tracking-wider text-blue-800 bg-blue-200 rounded-lg bg-opacity-50'>Not Submitted</span>
-                </td> 
-            </tr>
-
-            <tr className='bg-gray-50'>
-              <td  className='p-3 text-sm text-gray-700'>
-               <div className='flex items-center gap-2 '>
-                 <input type="checkbox" name='checkbox' id='checkbox' />
-                <FontAwesomeIcon className='mt-1 rounded-full bg-opacity-50 bg-yellow-300 px-2 py-2' icon={faLuggageCart}/>
-                  <div>
-                    <span>09/11/2022</span>
-                    <h4>Travel Expenses</h4>
-                  </div>
-                </div>
-              </td>
-              <td className='p-3 text-sm text-gray-700'>Airlines</td> 
-              <td className='p-3 text-sm text-gray-700'>450.20</td> 
-              <td className='p-3 text-sm text-gray-700'>November_2022</td> 
-              <td className='p-3 text-sm text-gray-700'>
-                <span className='p-1.5 font-medium text-xs uppercase -tracking-wider text-pink-800 bg-pink-200 rounded-lg bg-opacity-50'>Submitted</span>
-                </td> 
-            </tr>
-
-
-            <tr className='bg-white'>
-              <td className='p-3 text-sm text-gray-700'>
-               <div className='flex items-center gap-2'>
-                 <input type="checkbox" name='checkbox' id='checkbox' />
-                <FontAwesomeIcon className='mt-1 rounded-full bg-opacity-50 bg-yellow-300 px-2 py-2' icon={faBurger}/>
-                  <div>
-                    <span>09/11/2022</span>
-                    <h4>Client Dinner</h4>
-                  </div>
-                </div>
-               </td>  
-              <td className='p-3 text-sm text-gray-700'>Bistro</td> 
-              <td className='p-3 text-sm text-gray-700'>120.00</td> 
-              <td className='p-3 text-sm text-gray-700'>November_2022</td> 
-              <td className='p-3 text-sm text-gray-700' >
-                <span className='p-1.5 font-medium text-xs uppercase -tracking-wider text-blue-800 bg-blue-200 rounded-lg bg-opacity-50'>Not Submitted</span>
-                </td> 
-            </tr>
-
-            <tr className='bg-gray-50'>
-              <td className='p-3 text-sm text-gray-700'>
-                <div className='flex items-center gap-2'>
-                   <input type="checkbox" name='checkbox' id='checkbox' />
-                  <FontAwesomeIcon className='mt-1 rounded-full bg-opacity-50 bg-yellow-300 px-2 py-2' icon={faBed}/>
-                  <div>
-                    <span>09/11/2022</span>
-                    <h4>Accommodation</h4>
-                  </div>
-                </div>
-              </td>
-              <td className='p-3 text-sm text-gray-700'>Hotel***</td> 
-              <td className='p-3 text-sm text-gray-700'>275.75</td> 
-              <td className='p-3 text-sm text-gray-700'>November_2022</td> 
-              <td className='p-3 text-sm text-gray-700'>
-                <span className='p-1.5 font-medium text-xs uppercase -tracking-wider text-pink-800 bg-pink-200 rounded-lg bg-opacity-50'>Submited</span>
-                </td> 
-            </tr>
-
-            <tr className='bg-white'>
-              <td className='p-3 text-sm text-gray-700'>
-               <div className='flex items-center gap-2'>
-                 <input type="checkbox" name='checkbox' id='checkbox' />
-                <FontAwesomeIcon className='mt-1 rounded-full bg-opacity-50 bg-yellow-300 px-2 py-2' icon={faMobile}/>
-                  <div>
-                    <span>09/11/2022</span>
-                    <h4>News Subscription</h4>
-                  </div>
-                </div>
-              </td>
-              <td className='p-3 text-sm text-gray-700' >NewsTimes</td> 
-              <td className='p-3 text-sm text-gray-700' >30.00</td> 
-              <td className='p-3 text-sm text-gray-700'>November_2022</td> 
-              <td className='p-3 text-sm text-gray-700' >
-                <span className='p-1.5 font-medium text-xs uppercase -tracking-wider text-blue-800 bg-blue-200 rounded-lg bg-opacity-50'>Not Submited</span>
-                </td> 
-            </tr>
-          </tbody>
-        </table>
-      </div> */}
+    
+     
 
     </div>
   )
